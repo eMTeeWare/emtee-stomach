e5b7529ec838d0e4840a8068c5c2e4c830d9dd93
Merlin Thomas <m.thomas@steinberg.de>
2018-01-18 17:02:12 +0100

    Fix inclusion of subviews in main view

--- src/main/java/MainView.kt
+++ src/main/java/MainView.kt
@@ -1,18 +1,23 @@
+import javafx.scene.layout.BorderPane
 import tornadofx.*
+import tornadofx.InternalWindow.Styles.Companion.top
 
 /**
  * Created by MThomas on 18.01.2018.
  */
 class MainView : View("My View") {
-    override val root = borderpane {
-        top {
-            MainMenuBar()
-        }
-        left {
-            FoodConsumptionView()
-        }
-        center {
-            ConsumptionGraph()
+    override val root = BorderPane()
+    init {
+        with(root) {
+            top {
+                root += MainMenuBar()
+            }
+            left {
+                root += FoodConsumptionView()
+            }
+            center {
+                root += ConsumptionGraph()
+            }
         }
     }
 }
