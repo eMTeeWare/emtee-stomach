15f17ebc49cfe77ca0e4ad58b832779397059516
Merlin Thomas <m.thomas@steinberg.de>
2018-01-18 17:02:32 +0100

    Add minimal menu structure

--- src/main/java/MainMenuBar.kt
+++ src/main/java/MainMenuBar.kt
@@ -1,10 +1,23 @@
 import tornadofx.*
 import javafx.scene.control.MenuBar
+import tornadofx.FX.Companion.application
 
 /**
  * Created by MThomas on 18.01.2018.
  */
 class MainMenuBar : View("My View") {
-    override val root = menubar()
+    override val root = menubar {
+        menu("File") {
+            item("Load", "Shortcut+L").action {
+                println("Loading!")
+            }
+            item("Save","Shortcut+S").action {
+                println("Saving!")
+            }
+            item("Quit", "Shortcut+Q").action {
+                application.stop()
+            }
+        }
+    }
 
 }
