b4523c673b107404b3bbf4b8f0cbe061f0345da9
Merlin Thomas <m.thomas@steinberg.de>
2018-01-19 15:30:30 +0100

    Fill table with sample data

--- src/main/java/ui/FoodConsumptionView.kt
+++ src/main/java/ui/FoodConsumptionView.kt
@@ -2,9 +2,19 @@ package ui
 
 import model.DailyConsumptionData
 import tornadofx.*
+import java.time.LocalDate
+import java.time.Month
 
 class FoodConsumptionView : View() {
     override val root = tableview<DailyConsumptionData> {
+        items = listOf(
+                DailyConsumptionData(LocalDate.of(2018,Month.JANUARY, 1), 2000, "Nudeln mit Soße"),
+                DailyConsumptionData(LocalDate.of(2018,Month.JANUARY, 2), 1800, "Rehrücken")
+        ).observable()
+
+        column(messages["dateheader"], DailyConsumptionData::date)
+        column(messages["caloryheader"], DailyConsumptionData::consumption)
+        column(messages["maindishheader"], DailyConsumptionData::mainDish)
 
     }
 
