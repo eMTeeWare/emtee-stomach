ba482099ef7aee4766741ebfa38031e8e00af70c
Merlin Thomas <m.thomas@steinberg.de>
2018-01-19 09:28:57 +0100

    Introduce internalization

--- src/main/java/EmteeStomach.kt
+++ src/main/java/EmteeStomach.kt
@@ -1,5 +1,6 @@
 import tornadofx.*
 import ui.MainView
+import java.util.*
 
 class EmteeStomach: App(MainView::class) {
 }
--- src/main/java/ui/MainView.kt
+++ src/main/java/ui/MainView.kt
@@ -6,9 +6,11 @@ import tornadofx.*
 /**
  * Created by MThomas on 18.01.2018.
  */
-class MainView : View("My View") {
+class MainView : View() {
     override val root = BorderPane()
     init {
+        // this will not work in the View constructor
+        title = messages["appname"]
         with(root) {
             top {
                 root += MainMenuBar()
